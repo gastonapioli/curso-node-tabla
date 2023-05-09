5a3bc1bbb48267d06d70ab21be8250d4eeebf8aa
gastonapioli <gaston.apioli@telefonica.com>
2023-05-09 11:47:16 -0300

    ...

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const argv = require("./config/yargs");
 
 const [, base = 5] = arg3.split("="); //split separa elementos por el simbolo igual, oseas que tengo dos argumentos */
 
+
+
 console.log(argv);
 console.log("base: yargs", argv.b);
 
